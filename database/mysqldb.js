fcf1215775f9df7a52f33e6f884781efc5b24715
smartoryu <prikenang@gmail.com>
2020-04-11 04:09:02 +0700

    minor fix

--- database/mysqldb.js
+++ database/mysqldb.js
@@ -5,7 +5,6 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_MYSQL,
-  port: process.env.DB_PORT,
 });
 
 module.exports = db;
